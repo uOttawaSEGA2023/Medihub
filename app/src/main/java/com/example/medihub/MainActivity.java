5273340084894757b0006ea238d97683d7ba5d40
Charley Liu <charleyliu716@gmail.com>
2023-10-06 13:37:14 -0400

    added firebase auth sdk

--- app/src/main/java/com/example/medihub/MainActivity.java
+++ app/src/main/java/com/example/medihub/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.medihub;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.firebase.FirebaseApp;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
     }
 }
