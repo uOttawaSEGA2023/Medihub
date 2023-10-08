a2f1bfd3b838a6a5a43b652a62e56deeff37c0df
feftywacky <linfeiyu14@gmail.com>
2023-10-07 22:30:05 -0400

    doctor and patient controller and layout files

---
+++ app/src/main/java/com/example/medihub/PatientRegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.example.medihub;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PatientRegistrationActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_registration);
+    }
+}
