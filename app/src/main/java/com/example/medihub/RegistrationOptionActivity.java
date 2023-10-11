18b3227e66f546cc0cc98737d6b46cb7f1a176bf
feftywacky <linfeiyu14@gmail.com>
2023-10-11 17:48:55 -0400

    buttons working for option page

--- app/src/main/java/com/example/medihub/RegistrationOptionActivity.java
+++ app/src/main/java/com/example/medihub/RegistrationOptionActivity.java
@@ -1,16 +1,46 @@
 package com.example.medihub;
 
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.auth.FirebaseAuth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationOptionActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_option);
+        Button doctor_button = findViewById(R.id.buttonDoc);
+        Button patient_button = findViewById(R.id.buttonPatient);
+
+        doctor_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDocRegistration();
+            }
+        });
+
+        patient_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPatientRegistration();
+            }
+        });
+
+    }
+
+    public void startDocRegistration()
+    {
+        Intent intent = new Intent(this, DoctorRegistrationActivity.class);
+        startActivity(intent);
+    }
+
+    public void startPatientRegistration()
+    {
+        Intent intent = new Intent(this, PatientRegistrationActivity.class);
+        startActivity(intent);
     }
 }
