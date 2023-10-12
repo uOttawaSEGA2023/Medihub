950d064b24ceb8cd5000715d72400128da637fc0
Charley Liu <charleyliu716@gmail.com>
2023-10-12 19:31:04 -0400

    added doctor ui registration page

--- app/src/main/java/com/example/medihub/DoctorRegistrationActivity.java
+++ app/src/main/java/com/example/medihub/DoctorRegistrationActivity.java
@@ -3,11 +3,26 @@ package com.example.medihub;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+import com.example.medihub.enums.DoctorSpecialty;
+
 public class DoctorRegistrationActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_registration);
+
+        // get specialties layout
+        LinearLayout specialtiesLayout = findViewById(R.id.specialties);
+
+        // dynamically add doctor specialties as checkboxes to specialties layout
+        for (DoctorSpecialty spec : DoctorSpecialty.values()) {
+            CheckBox checkBox = new CheckBox(this);
+            checkBox.setText(spec.toString().replaceAll("_", " "));
+            specialtiesLayout.addView(checkBox);
+        }
     }
 }
--- app/src/main/java/com/example/medihub/models/PatientProfile.java
+++ app/src/main/java/com/example/medihub/models/PatientProfile.java
@@ -5,21 +5,46 @@ import com.example.medihub.enums.UserRole;
 import java.util.ArrayList;
 
 public class PatientProfile extends UserProfile {
-    // CONSTRUCTORS
+    // INSTANCE VARIABLES
+    private String healthCardNumber;
 
-    public PatientProfile(UserRole role, String firstName, String lastName, String healthCardNumber, String address, String phoneNumber) {
-        super(role, firstName, lastName, healthCardNumber, address, phoneNumber);
+    // health card number regex obtained from ontario gov website:
+    // https://health.gov.on.ca/en/pro/publications/ohip/ebs_hcv_specs.aspx#:~:text=The%20health%20number%20is%20a,face%20of%20every%20health%20card.&text=Version%20code%20is%20an%20alphabetic,red%20and%20white%E2%80%9D)%20cards.
+    private static final String HEALTH_CARD_REGEX = "[1-9]\\d{9}";
+
+
+    // CONSTRUCTORS
+    public PatientProfile(UserRole role, String firstName, String lastName, String address, String phoneNumber, String healthCardNumber) {
+        super(role, firstName, lastName, address, phoneNumber);
+        this.healthCardNumber = healthCardNumber;
+    }
+
+    // GETTERS
+    public String getHealthCardNumber() {
+        return healthCardNumber;
+    }
+
+    // SETTERS
+    public void setHealthCardNumber(String healthCardNumber) {
+        this.healthCardNumber = healthCardNumber;
     }
 
     // METHODS
 
     @Override
     public String toString() {
-        return super.toString();
+        return super.toString() +
+                "\nHealth Card Number: " + healthCardNumber;
     }
 
     @Override
     public ArrayList<String> validate() {
-        return super.validate();
+        ArrayList<String> errors = super.validate();
+
+        if (healthCardNumber == null || !healthCardNumber.matches(HEALTH_CARD_REGEX)) {
+            errors.add("invalid health card number (format: only digits no dashes)");
+        }
+
+        return errors;
     }
 }
