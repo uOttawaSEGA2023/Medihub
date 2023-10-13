23b3a65a75e9e37c467f893dec408e3e63a4fda7
Charley Liu <charleyliu716@gmail.com>
2023-10-13 08:58:20 -0400

    refactored variable

--- app/src/main/java/com/example/medihub/LoginActivity.java
+++ app/src/main/java/com/example/medihub/LoginActivity.java
@@ -2,22 +2,17 @@ package com.example.medihub;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import com.google.firebase.auth.AuthResult;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import android.util.Log;
 
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private Button button5;
+    private Button buttonCreateAccount;
     private Button buttonLogin;
     private EditText textEmail;
     private EditText textPassword;
@@ -28,11 +23,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        button5 = findViewById(R.id.button5);
+        buttonCreateAccount = findViewById(R.id.buttonCreateAccount);
         textEmail = findViewById(R.id.textEmail);
         textPassword = findViewById(R.id.textPassword);
 
-        button5.setOnClickListener(new View.OnClickListener() {
+        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
