73333f9786ceb839df040e8abd826188b2152ed6
ChristopherLit <litchristopher@gmail.com>
2023-10-14 00:35:39 -0400

    Created a welcome page
    Linked the login button to the welcome page

---
+++ app/src/main/java/com/example/medihub/WelcomeActivity.java
@@ -0,0 +1,24 @@
+package com.example.medihub;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.medihub.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Log.d("test", "123");
+    }
+
+
+}
