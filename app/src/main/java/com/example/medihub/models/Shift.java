e8670d60f0f9447aa149d02cbe4e0fc3b99de24d
Charley Liu <charleyliu716@gmail.com>
2023-11-02 00:10:20 -0400

    add barebones shift class

---
+++ app/src/main/java/com/example/medihub/models/Shift.java
@@ -0,0 +1,56 @@
+package com.example.medihub.models;
+
+import com.example.medihub.interfaces.Model;
+
+import java.util.Date;
+import java.util.HashMap;
+
+public class Shift implements Model {
+    private String key;
+    private String doctor_id;
+    private Date startDate;
+    private Date endDate;
+
+
+
+    public Shift() {}
+
+    public Shift(String doctor_id, Date startDate, Date endDate) {
+        this.doctor_id = doctor_id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+
+    public String getDoctor_id() {
+        return doctor_id;
+    }
+    public Date getStartDate() {
+        return startDate;
+    }
+    public Date getEndDate() {
+        return endDate;
+    }
+
+
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public HashMap<String, String> validate() {
+        // TODO: Make sure start and end dates are same day and that end is not before start
+        // TODO (maybe): validate that reference of patient_id is a patient
+        // TODO (maybe): validate that reference of doctor_id is a doctor
+
+        HashMap<String, String> errors = new HashMap<>();
+        return errors;
+    }
+}
